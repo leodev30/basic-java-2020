823637870156d34b2cef5613cabb85e5113332dc
Zleozpham <45645698+Zleozpham@users.noreply.github.com>
2020-04-11 23:18:42 +0700

    Create HelloWorld.java

---
+++ Lab00/HelloWorld.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class HelloWorld{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		System.out.println("Hello,world!");
+		System.out.println("So vua nhap la: "+a);	
+	}
+	
+}
