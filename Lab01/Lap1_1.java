445240c8fc97b27181f3078f1e90fd2fccde4fd9
Zleozpham <45645698+Zleozpham@users.noreply.github.com>
2020-04-11 23:20:11 +0700

    Create Lap1_1.java

---
+++ Lab01/Lap1_1.java
@@ -0,0 +1,12 @@
+public class Lap1_1{
+	public void InThongTin(){
+		System.out.println("Pham Ngoc Tuan \t LT0718a \t K63J \t tuanpn@fimo.edu.vn");
+		System.out.println("9 bottles of beer on the wall, 9 bottles of beer. \nTake one down, pass it around,\n8 bottles of beer on the wall, 8 bottles of beer.\nTake one down, pass it around, \n... \n1 bottle of beer on the wall, 1 bottle of beer.\nTake one down, pass it around,\nNo more bottles of beer on the wall.");	
+	}
+	public static void main(String[] args){
+		Lap1_1 test = new Lap1_1();
+		test.InThongTin();
+		
+	}
+	
+}
