76e48bdb2d4dc18927b5dcba80a718e59b102700
Zleozpham <45645698+Zleozpham@users.noreply.github.com>
2020-04-11 23:16:31 +0700

    Create Bai2_1.java

---
+++ Lab00/Bai2_1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Bai2_1{
+	public static void main(String[] args){
+		int i;
+		int sum = 0;
+		for(i=0;i<10;i++){
+			sum = sum+i;
+		}
+		System.out.println("Tong cua muoi so dau tien la "+sum);
+	}
+}
