5608269a47f9aaddebfc0d1b43753640ed4dc3c1
Zleozpham <45645698+Zleozpham@users.noreply.github.com>
2020-04-20 19:13:04 +0700

    Update Main.java

--- Lab06a/src/com/company/Main.java
+++ Lab06a/src/com/company/Main.java
@@ -9,6 +9,10 @@ public class Main{
             Shape cir1 = new Circular();
             Shape cir2 = new Circular();
             Composite com = new Composite();
+            Shape lineC = new Line();
+            Shape cirC = new Circular();
+            com.add(lineC);
+            com.add(cirC);
             figuer.add(line);
             figuer.add(cir1);
             figuer.add(cir2);
