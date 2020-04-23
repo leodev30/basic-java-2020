d337803c8d6dd7d294950fb0a15e2d681c831c88
Zleozpham <45645698+Zleozpham@users.noreply.github.com>
2020-04-23 15:03:21 +0700

    Create Line.java

---
+++ DaHinh/src/Lab06/src/Line.java
@@ -0,0 +1,30 @@
+public class Line extends Shape {
+    protected  int length = 10;
+    Line(){
+
+    }
+    @Override
+    public void draw() {
+        System.out.println("This is Line");
+    }
+
+    @Override
+    public void rotate(int angle) {
+
+        System.out.println("this is rotate"+ angle);
+    }
+
+    @Override
+    public void moveLeft(int point) {
+
+        System.out.println("move Left "+ point);
+    }
+
+    @Override
+    public void setColor(int color) {
+        System.out.println("Set color Line "+ color);
+    }
+    public int getLength(){
+        return length;
+    }
+}
