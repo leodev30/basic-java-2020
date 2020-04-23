919fc37b85418b6ae8c2de4f3ea17d7d46638820
Zleozpham <45645698+Zleozpham@users.noreply.github.com>
2020-04-23 11:04:45 +0700

    Create Gambler.java

---
+++ DaHinh/src/Lab07/src/Gambler.java
@@ -0,0 +1,28 @@
+import java.util.Random;
+
+public class Gambler extends Account{
+    Gambler(float balance) {
+        super(balance);
+    }
+
+    @Override
+    public void withdraw(float amount) {
+        Random rn = new Random();
+        double d = rn.nextDouble();
+        if(d<=0.49){
+            balance = balance - 0 * amount;
+            transactions++;
+        } else{
+            balance = balance - 2 * amount;
+            transactions++;
+
+        }
+
+    }
+
+    @Override
+    public void endMonthCharge() {
+        balance = balance - 5;
+        transactions++;
+    }
+}
