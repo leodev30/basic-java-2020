01dfe059ae1c6c851464095d36a8a8373f24861a
Zleozpham <45645698+Zleozpham@users.noreply.github.com>
2020-04-23 15:03:14 +0700

    Create Shape.java

---
+++ DaHinh/src/Lab06/src/Shape.java
@@ -0,0 +1,10 @@
+public abstract class Shape {
+    public int color;
+    public Shape(){
+
+    }
+    public abstract void draw();
+    public abstract  void rotate(int angle);
+    public abstract  void moveLeft(int point);
+    public abstract  void setColor(int color);
+}
