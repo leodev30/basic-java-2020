70470c1fc66b5d6f6dedde729265387569c1657a
Zleozpham <45645698+Zleozpham@users.noreply.github.com>
2020-04-23 15:03:24 +0700

    Create Composite.java

---
+++ DaHinh/src/Lab06/src/Composite.java
@@ -0,0 +1,46 @@
+import java.util.List;
+import java.util.ArrayList;
+public class Composite extends Shape {
+    List<Shape> listShape = new ArrayList<>();
+    Composite(){
+
+    }
+    @Override
+    public void draw() {
+        System.out.println("draw");
+
+    }
+
+    @Override
+    public void rotate(int angle) {
+
+        System.out.println("rotate composite "+ angle);
+    }
+
+    @Override
+    public void moveLeft(int point) {
+
+        System.out.println("move Left composite"+ point);
+
+    }
+
+    @Override
+    public void setColor(int color) {
+
+        System.out.println("Set color composite "+ color);
+
+    }
+    public void add(Shape shape){
+        listShape.add(shape);
+    }
+    public void remove(Shape shape){
+        for(Shape s : listShape){
+            if(s.equals(shape)){
+                listShape.remove(shape);
+            }
+        }
+    }
+    public void getChild(int id){
+        listShape.get(id);
+    }
+}
