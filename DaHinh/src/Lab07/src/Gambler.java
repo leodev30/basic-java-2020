377fcc839cc9cc91e0492b234d233b1a2fbf949b
Zleozpham <45645698+Zleozpham@users.noreply.github.com>
2020-04-27 20:30:28 +0700

    Update Gambler.java

--- DaHinh/src/Lab07/src/Gambler.java
+++ DaHinh/src/Lab07/src/Gambler.java
@@ -8,7 +8,8 @@ public class Gambler extends Account{
     @Override
     public void withdraw(float amount) {
         Random rn = new Random();
-        double d = rn.nextDouble();
+        double d = rn.nextDouble();//từ 0 đến 1
+        System.out.println(d);
         if(d<=0.49){
             balance = balance - 0 * amount;
             transactions++;
