74afdaf5c1a40fd5edf7ce8f99e813eb99b96474
Zleozpham <45645698+Zleozpham@users.noreply.github.com>
2020-05-11 20:14:43 +0700

    Create ObjectInputStreamExample.java

---
+++ Serializable/src/ObjectInputStreamExample.java
@@ -0,0 +1,23 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+public class ObjectInputStreamExample {
+    public static void main(String[] args) throws IOException {
+        ObjectInputStream ois = null;
+
+        try {
+            ois = new ObjectInputStream(new FileInputStream("testout.txt"));
+            // read student
+            Student student = (Student) ois.readObject();
+            // show student
+            System.out.println(student.toString());
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            ois.close();
+        }
+    }
+}
